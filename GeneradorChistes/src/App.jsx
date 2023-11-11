7bcd2d49d8029debc017ad607dd0e4510ec6f00f
Claudia Barajas <claudiabarajas76@gmail.com>
2023-11-11 01:04:35 -0600

    segundo commit

--- GeneradorChistes/src/App.jsx
+++ GeneradorChistes/src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
+  const [chistes, setchistes] = useState([])
+  const [ultimoChiste, setultimoChiste] = useState()
+
+  const obtenerChiste = async () => {
+
+    const response = await axios("https://icanhazdadjoke.com/", {
+      headers: {
+        Accept: "application/json",
+      },
+    });
+    const data = response.data;
+    data.status === 200 && setultimoChiste(data)
+  }
+ 
+  useEffect(() => {
+    obtenerChiste()
+  }, [])
+
+  useEffect(() => {
+    ultimoChiste !== undefined && setchistes([...chistes, ultimoChiste])
+  }, [ultimoChiste])
+
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <button onClick={obtenerChiste}>Añadir</button>
+       <div>
+        {chistes.map(chiste => (
+          <h4 key={chiste.id}>{chiste.joke}</h4>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      
+    </div>
   )
 }
-
-export default App
